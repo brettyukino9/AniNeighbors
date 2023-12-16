40e5c0a80bd76c999a92d82d9b4dab3e62040090
bpreier <bpreier@ncsu.edu>
2023-12-15 20:20:42 -0500

    Implemented shared 10s

--- AniNeighbors.py
+++ AniNeighbors.py
@@ -25,6 +25,7 @@ from pip._vendor import requests
 import sys
 import math
 import pandas as pd
+import csv
 
 
 map = {}
@@ -44,11 +45,11 @@ url = 'https://graphql.anilist.co'
 
 # Make the HTTP Api request to get the user id of the username
 response = requests.post(url, json={'query': queryUserId, 'variables': variablesUserId}).json()
-userId = response['data']['User']['id']
+global_user_id = response['data']['User']['id']
 
 # Define our query variables and values that will be used in the query request
 variables = {
-    'userId': userId
+    'userId': global_user_id
 }
 
 queryUserList = '''
@@ -67,9 +68,6 @@ query {
 }
 
 '''
-
-print(userId)
-
 # Query to get all following 
 # {
 #   Page(page: 1) {
@@ -83,12 +81,15 @@ print(userId)
 # }
 
 AnimeLists = {}
+global_user_anime_count = -1
+
 def makeUserDFFromResponse(response, userId):
+    global global_user_anime_count
     list_owner = response['data']['User']['name']
     anime_count = 0
     for animelist in response['data']['MediaListCollection']['lists']:
         anime_count += len(animelist['entries'])
-    if(anime_count < 10):
+    if(global_user_anime_count and anime_count < global_user_anime_count * 0.15):
         return -1
     userList = pd.DataFrame(columns='userid, title, media_id, score, status'.split(', '))
     for animelist in response['data']['MediaListCollection']['lists']:
@@ -98,6 +99,9 @@ def makeUserDFFromResponse(response, userId):
             if(anime['scoreRaw'] == 0):
                 continue
             userList.loc[anime['mediaId']] = [userId, anime['media']['title']['romaji'], anime['mediaId'], anime['scoreRaw'], anime['status']]
+    # if(userId != global_user_id):
+    if(userId == global_user_id):
+        global_user_anime_count = anime_count
     AnimeLists[list_owner] = userList
     return userList
 
@@ -131,7 +135,7 @@ def getUserListFromAPI(userId):
     userList = makeUserDFFromResponse(response, userId)
     return userList        
 
-userList = getUserListFromAPI(userId)
+userList = getUserListFromAPI(global_user_id)
 
 
 for i in range(1, 3) :
@@ -140,10 +144,35 @@ for i in range(1, 3) :
 
 print(AnimeLists.keys())
 
-stats = pd.DataFrame(columns='name, anime_count, meanscore, hoh, pearson, shared10s'.split(', '))
+def getShared10s(userList):
+    with open("shared 10s.csv", newline="", encoding='latin1') as csvfile:
+        rows = csv.reader(csvfile, delimiter=",", dialect="excel")
+        shared_10s = 0
+        total_10s = 0
+        userList.sort_values(by='score_x')
+        average = userList['score_x'].mean()
+        stdev = userList['score_x'].std()
+        userList['zscore'] = (userList['score_x'] - average) / stdev
+        userList = userList.sort_values(by='zscore', ascending=False)
+        normalizedUserList = userList[userList['zscore'] > 0.85]
+        for row in rows :
+            total_10s += 1
+            anime = row[0]
+            # Search for an entry of anime in any entry of title_x
+            for index, row in normalizedUserList.iterrows():
+                if(anime.lower() in row['title_x'].lower()):
+                    shared_10s += 1
+                    break
+    
+    return shared_10s, total_10s
+
+stats = pd.DataFrame(columns='name, anime_count, merged mean, reduces mean by, hoh, pearson, shared10s'.split(', '))
 for username in AnimeLists:
     list = AnimeLists[username]
     list_owner = username
+
+    # Get the average of the user BEFORE merging
+    user_list_old_average = userList['score'].mean()
 
     # Attach scores from userList that match the mediaId of the filtered_responses
     merged_data = pd.merge(list, userList, on='media_id', how='left')
@@ -163,9 +192,17 @@ for username in AnimeLists:
 
     # Find the number of anime the user has watched
     anime_count = len(merged_data)
-    
+
+    # Find the amount of shared 10s
+    shared_10s, total_10s = getShared10s(merged_data)
+    print(shared_10s, total_10s)
+
+    # Find the users new mean after merging
+    user_list_new_average = merged_data['score_y'].mean()
+    mean_diff = user_list_old_average - user_list_new_average
+
     ratings_df = merged_data[['score_x', 'score_y']]
     pearson = ratings_df.corr(method='pearson')['score_x']['score_y']
     print(pearson)
-    stats.loc[list_owner] = [list_owner, anime_count, meanscore, 0, pearson, 0]
+    stats.loc[list_owner] = [list_owner, anime_count, meanscore, mean_diff, 0, pearson, shared_10s]
     print(stats)
